7cf003400324a78431a364d102ea39ea1b4177e8
Monk <forgetmenaught@gmail.com>
2016-10-20 16:06:56 -0700

    build the rest of admin crud for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,17 +1,38 @@
 class Admin::CategoriesController < Admin::ApplicationController
   def new
+    @page_title = "Add Category"
+    @category = Category.new
   end
 
   def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:notice] = "Category created!"
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Updated successfully!"
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice] = "Category removed."
+    redirect_to admin_categories_path
   end
 
   def index
@@ -19,5 +40,10 @@ class Admin::CategoriesController < Admin::ApplicationController
   end
 
   def show
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
